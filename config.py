8ad2f4424e72230595169bfd89b2fb0387323532
Ji19283756 <67918859+Ji19283756@users.noreply.github.com>
2021-04-19 21:45:35 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+
+# Path of the chromedriver
+chrome_driver_path = r"C:\Users\Kedde\AppData\Local\Temp\Temp1_chromedriver_win32.zip\chromedriver.exe"
+
+# Time (Minutes) of how long the program will wait to check if the downloaded file exists, if it is not found in this
+# time, the download will terminate
+file_checks_amount = 60
+
+# The number of times the program will search the name on Mp3Juices before shortening the name
+repeats_before_shortening = 5
+
+# number of times the program will search up the song and wait for the download button to show up
+tries = 100
